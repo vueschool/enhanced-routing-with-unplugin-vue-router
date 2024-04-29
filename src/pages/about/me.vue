1f48f949c119552de04e066378db019fd180b44f
Daniel Kelly <me@danielkelly.io>
2024-04-29 17:18:37 -0500

    lesson-4-file-based-routing

---
+++ src/pages/about/me.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about-me">
+    <h1>This is an about me page</h1>
+  </div>
+</template>
--- typed-router.d.ts
+++ typed-router.d.ts
@@ -19,6 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
-    '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
+    '/about/': RouteRecordInfo<'/about/', '/about', Record<never, never>, Record<never, never>>,
+    '/about/me': RouteRecordInfo<'/about/me', '/about/me', Record<never, never>, Record<never, never>>,
   }
 }
