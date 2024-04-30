2782a87005b8fb12005983f9a81d15c58db0a5d4
Daniel Kelly <me@danielkelly.io>
2024-04-30 15:49:18 -0500

    lesson-11-navigation-guards-route-meta-and-definepage

--- src/pages/about/me.vue
+++ src/pages/about/me.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { definePage } from "unplugin-vue-router/runtime";
+
+definePage({
+  alias: ["/me"],
+  meta: {
+    requiresAuth: true,
+  },
+});
+</script>
 <template>
   <div class="about-me">
     <h1>This is an about me page</h1>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,4 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 });
 
+router.beforeEach((to, from) => {
+  const loggedIn = false;
+  if (to.meta.requiresAuth && !loggedIn) {
+    router.replace({ name: "/" });
+  }
+});
+
 export default router;
